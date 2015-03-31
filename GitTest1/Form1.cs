8b4e0e0ac7c4396b1f56d9e0dcce64e6ddb27914
RiverTaig <River.taig@Gmail.com>
2015-03-31 15:46:59 -0600

    Changed to Hello World
    Fixed spelling

--- GitTest1/Form1.cs
+++ GitTest1/Form1.cs
@@ -18,7 +18,7 @@ namespace GitTest1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello Werld");
+            MessageBox.Show("Hello World");
         }
     }
 }
